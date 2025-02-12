7af2fbb5c1b6b6e59ed7a62b117dc977af65e3db
Dadan Hidayat <dadanhidyt@gmail.com>
2025-02-12 18:33:27 +0700

    feat(database): :memo: drizzle configure additional

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,29 @@
+import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
+import type { Logger } from "drizzle-orm";
+import { info } from "console";
+
+const dbLoger: Logger = {
+  logQuery(query, params) {
+    info(`[Drizzle Error]`, params, query);
+  },
+};
 
 config({
   path: ".env",
 });
+if (!process.env.DATABASE_URL) {
+  throw new Error("[DATABASE_URL] tidak ada di .env");
+}
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL!, {
+  fetchOptions: {
+    cache: "no-store",
+    connectionAttempts: 3, // Coba ulang koneksi jika gagal
+  },
+});
+
+export const db = drizzle(sql, {
+  logger: process.env.NODE_ENV === "development" ? dbLoger : undefined,
+});
